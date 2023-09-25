89b08f9df18dfb57225e9d7d2c7aa4e5ef02cff1
kanwarpreetsachdeva <kanwarpreet.sachdeva@gmail.com>
2023-09-25 15:41:04 -0700

    Update streamlit_app.py
    fixed minor tab issue on line 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
- import streamlit
+import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -17,5 +17,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index) , ['Avocado', 'Strawberries'])
 
-# Display the table on the page.
+# Display the table on the page
 streamlit.dataframe(my_fruit_list)
