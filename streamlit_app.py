cd5d45a3a2ef141976b3de6a2f38fb6ad822f4bc
kanwarpreetsachdeva <kanwarpreet.sachdeva@gmail.com>
2023-10-02 11:19:36 -0700

    Update streamlit_app.py
    Added function to add new fruit to fruit_load_list based on user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ except URLError as e:
 
 #Snowflake related functions
 def get_fruit_load_list():
-  my_cur = my_cnx.cursor()
+  with my_cnx.cursor() as my_cur
   my_cur.execute("select * from fruit_load_list")
   return my_cur.fetchall()
 
@@ -57,11 +57,16 @@ if streamlit.button('Get Fruit Load list'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-# Do not execute steps beyond this point till I debug
-streamlit.stop()
-
 # Ask the user to add a fruit
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur
+  my_cur.execute("insert into fruit_load_list values ('from snowflake')")
+  return 'Thanks for adding " + new_fruit
+  
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write("Thanks for adding", add_my_fruit)
+if streamlit.button ('Add a fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.write(back_from_function)
 
-my_cur.execute("insert into fruit_load_list values ('from snowflake')")
+
