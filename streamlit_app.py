c0afffde1f6b2eccfefd1c575814134e36163330
kanwarpreetsachdeva <kanwarpreet.sachdeva@gmail.com>
2023-10-02 11:06:37 -0700

    Update streamlit_app.py
    Added button on screen. When pressed, prints fruit load list from Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,17 +46,24 @@ try:
 except URLError as e:
   streamlit.error()
 
-# Do not execute steps beyond this point till I debug
-streamlit.stop()
-
-
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake related functions
+def get_fruit_load_list():
+  my_cur = my_cnx.cursor()
+  my_cur.execute("select * from fruit_load_list")
+  return my_cur.fetchall()
+
+if streamlit.button('Get Fruit Load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+# Do not execute steps beyond this point till I debug
+streamlit.stop()
 
 # Ask the user to add a fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
