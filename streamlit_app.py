bd6641cfd328eb23b7e3a1b947db43b87dbb2a28
kanwarpreetsachdeva <kanwarpreet.sachdeva@gmail.com>
2023-10-02 11:09:52 -0700

    Update streamlit_app.py
    Removed code calling my_cnx at wrong place

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,11 +46,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-#import snowflake.connector
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
 #Snowflake related functions
 def get_fruit_load_list(my_cnx):
   my_cur = my_cnx.cursor()
